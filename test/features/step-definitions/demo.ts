c3a8fe13c2bf8f8071cb94ae7a82d62b022d67c5
Andre Reynaldi Lusikooy <reynaldi@outlook.jp>
2023-01-15 00:38:17 +0700

    update coverage

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -30,26 +30,85 @@ Then(/^URL should match (.*)$/, async function (expectedURL) {
 Given(/^A web page is opened$/, async function () {
     await browser.url("https://www.amazon.com.au")
     await browser.setTimeout({ implicit: 15000, pageLoad: 10000 })
+    await browser.maximizeWindow()
 })
 
 When(/^Perfom web interactions$/, async function () {
    
-    await browser.execute(() => {
-        window.scrollBy(0, window.innerHeight)
-    })
-    await browser.pause(2000)
-     await browser.execute(() => {
-        window.scrollBy(0, -window.innerHeight)
-    })
-    await browser.pause(2000)
-    await browser.execute(() => {
-        window.scrollTo(0, document.body.scrollHeight)
-    })
+        let num = 12345
+        let strNum = num.toString()
 
-    await browser.pause(2000)
-    await browser.execute(() => {
-        window.scrollTo(0, document.body.scrollTop)
-    })
+        let ele = await $(`[type=number]`)
+        await ele.click()
+        for (let i = 0; i < strNum.length; i++) {
+            let charStr = strNum.charAt(i)
+            await browser.pause(1000)
+            await browser.keys(charStr)
+        }
+
+    // assert default
+    let ele = await $('//select/option[@selected="selected"]')
+    let val = ele.getText()
+    chai.expect(val).to.equal("Please select an option")
+    await browser.debug()
+
+    // select by att
+    let ddEle = await $('#dropdown')
+    await ddEle.selectByIndex(2)
+
+    /// get list
+    let eleArr = await $$(`select > option`)
+    let arr = []
+    for (let i = 0; i < eleArr.length; i++) {
+        let ele = eleArr[i]
+        let val = await ele.getText()
+        arr.push(val)
+    }
+    console.log(`>> Options Array:|${arr}|`);
+
+    await $(`=Click Here`).click()
+    await $(`=Elemental Selenium`).click()
+    let currentWinTitle = await browser.getTitle()
+    let parentWinHanle = await browser.getWindowHandle()
+    console.log(`>> currentWinTitle: ${currentWinTitle}`);
+
+    // // Switch window
+    let winHandles = await browser.getWindowHandles()
+    for (let i = 0; i < winHandles.length; i++){
+        console.log(`>> Win handle: ${winHandles[i]}`);
+        await browser.switchToWindow(winHandles[i])
+        currentWinTitle = await browser.getTitle()
+        if (currentWinTitle === "Elemental Selenium: Receive a Free, Weekly Tip on Using Selenium like a Pro"){
+            await browser.switchToWindow(winHandles[i])
+            let headerTxtEleSel = await $(`<h1>`).getText()
+            console.log(`>> headerTxtEleSel: ${headerTxtEleSel}`);
+            break
+        }
+    }
+
+    await $(`button=Click for JS Prompt`).click()
+    if (await browser.isAlertOpen()) {
+        let alertText = await browser.getAlertText()
+        console.log(`>> alertText: ${alertText}`);
+        await browser.sendAlertText(`Hello JS Prompt...`)
+        await browser.acceptAlert()
+        await browser.pause(2000)
+    }
+
+    // file upload
+    await $(`#file-upload`).addValue(`${process.cwd()}/reinskywalker/test/fileupload/dummy.txt`)
+    await $('#file-submit').click()
+
+   
+    await $(`=iFrame`).click()
+    let ele = await $(`#mce_0_ifr`)
+    await browser.switchToFrame(ele)
+    await $(`#tinymce`).click()
+    await browser.keys(["Meta", "A"])
+    await browser.pause(1000)
+    await browser.keys("Delete")
+    await $(`#tinymce`).addValue(`Typing into a frame...`)
+    await browser.switchToParentFrame()
     await browser.debug()
 
 })
